871dd26430ddff01e4c60324f3ed6d679959a5b1
donjoe <giumontanaro@gmail.com>
2021-03-09 16:38:24 +0100

    Render form inside modal; handle its funcionality with prop instead of internal state

--- src/components/CreatePostForm/index.js
+++ src/components/CreatePostForm/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
+import './CreatePostForm.css'
 
-const CreatePostForm = ({ onCreatePost }) => {
-    const [isOpen, setIsOpen] = useState(false)
+const CreatePostForm = ({ mode, isOpen, onToggleForm, onCreatePost }) => {
     const [formValues, setFormValues] = useState({
         title: '',
         description: '',
@@ -45,44 +45,63 @@ const CreatePostForm = ({ onCreatePost }) => {
 
     return (
         <>
-            <button className="btn btn-outline-primary" onClick={() => setIsOpen(!isOpen)}>Toggle form</button>
+            <button className="btn btn-outline-primary" onClick={onToggleForm}>Toggle form</button>
             {isOpen && (
-                <form onSubmit={handleCreatePost} className="mt-3">
-                    <div className="form-group">
-                        <label htmlFor="title">Title</label>
-                        <input
-                            id="title"
-                            required
-                            type="text"
-                            value={formValues.title}
-                            onChange={handleFormValues('title')}
-                            className="form-control"
-                        />
-                    </div>
+                <div className="modal">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div 
+                                className="modal-header" 
+                                style={{display: 'flex', justifyContent: 'flex-end'}}>
+                                <button
+                                    type="button"
+                                    onClick={onToggleForm}
+                                    className="btn btn-outline-primary"
+                                    aria-label="Close">
+                                        X
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <form onSubmit={handleCreatePost} className="mt-3">
+                                    <div className="form-group">
+                                        <label htmlFor="title">Title</label>
+                                        <input
+                                            id="title"
+                                            required
+                                            type="text"
+                                            value={formValues.title}
+                                            onChange={handleFormValues('title')}
+                                            className="form-control"
+                                        />
+                                    </div>
 
-                    <div className="form-group">
-                        <label htmlFor="description">Description</label>
-                        <textarea
-                            id="description"
-                            rows="4"
-                            value={formValues.description}
-                            onChange={handleFormValues('description')}
-                            className="form-control"
-                        />
-                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="description">Description</label>
+                                        <textarea
+                                            id="description"
+                                            rows="4"
+                                            value={formValues.description}
+                                            onChange={handleFormValues('description')}
+                                            className="form-control"
+                                        />
+                                    </div>
 
-                    <div className="form-group">
-                        <label htmlFor="url">URL</label>
-                        <input
-                            id="url"
-                            type="url"
-                            value={formValues.url}
-                            onChange={handleFormValues('url')}
-                            className="form-control"
-                        />
+                                    <div className="form-group">
+                                        <label htmlFor="url">URL</label>
+                                        <input
+                                            id="url"
+                                            type="url"
+                                            value={formValues.url}
+                                            onChange={handleFormValues('url')}
+                                            className="form-control"
+                                        />
+                                    </div>
+                                    <button className="btn btn-info">{mode.toUpperCase()}</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                    <button className="btn btn-info">Create Post</button>
-                </form>
+                </div>
             )}
         </>
     )
